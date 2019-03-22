4a73775db8adda8a2ada60e71673b6f2230c57da
Songze <songzeli8824@gmail.com>
2019-03-22 15:15:10 -0700

    coded merkle tree.

--- coded_merkle_tree.py
+++ coded_merkle_tree.py
@@ -2,3 +2,13 @@ import copy
 import poly_utils
 import rlp
 
+
+# Every point is an element of GF(2**16), so represents two bytes
+POINT_SIZE = 2
+# Every symbol contains 128 points
+POINTS_IN_SYMBOL = 128
+# A symbol is 256 bytes
+SYMBOL_SIZE = POINT_SIZE * POINTS_IN_SYMBOL
+
+
+
