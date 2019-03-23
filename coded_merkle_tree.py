fddbdc6a40294e37f6e49c45dc8150acbbc90c62
Songze <songzeli8824@gmail.com>
2019-03-22 18:42:13 -0700

    coded merkle tree construction

--- coded_merkle_tree.py
+++ coded_merkle_tree.py
@@ -1,14 +1,75 @@
 import copy
-import poly_utils
 import rlp
 
+try:
+    from Crypto.Hash import keccak
+    sha3 = lambda x: keccak.new(digest_bits=256, data=x).digest()
+except ImportError:
+    import sha3 as _sha3
+    sha3 = lambda x: _sha3.sha3_256(x).digest()
 
-# Every point is an element of GF(2**16), so represents two bytes
-POINT_SIZE = 2
-# Every symbol contains 128 points
-POINTS_IN_SYMBOL = 128
-# A symbol is 256 bytes
-SYMBOL_SIZE = POINT_SIZE * POINTS_IN_SYMBOL
+# A hash has 32 bytes
+HASH_SIZE = 32
+
+# We choose each symbol of the original block to have 256 bytes
+SYMBOL_SIZE = 256
+
+# number of hashes to aggregate to form a new symbol
+C = 8
+
+#Coding rate
+rate = 0.5
+
+#number of symbols reduces by reduce_factor each layer upward
+reduce_factor = C * rate
+
+
+# Returns the smallest power of reduce_factor equal to or greater than a number
+def higher_power_of_2(x):
+    higher_power_of_2 = 1
+    while higher_power_of_2 < x:
+        higher_power_of_2 *= 2
+    return higher_power_of_2
+
+def pad(data):
+    med = rlp.encode(data)
+    x = 1
+    while x * SYMBOL_SIZE * rate < len(med):
+    	x *= reduce_factor
+    return med + b'\x00' * (x * SYMBOL_SIZE * rate - len(med))
+
+def LDPC_encoding(symbols,rate):
+
+
+def hashAggregate(coded_symbols):
+	hashes = [sha3(x) for x in coded_symbols]
+	symbols = [hashes[i: i + C] for i in range(0, len(hashes), HASH_SIZE)]
+
+
+class Coded_merkle_tree:
+    def __init__(self, data, level):
+    	pdata = pad(data)
+    	symbols = [pdata[i: i + SYMBOL_SIZE] for i in range(0, len(pdata), SYMBOL_SIZE)]
+    	coded_symbols = LDPC_encoding(symbols,rate)
+    	tree = [coded_symbols]
+    	for j in range(0,level-1):
+    		symbols = hashAggregate(tree[j])
+    		tree.append(LDPC_encoding(symbols,rate))
+    	roots = [sha3(x) for x in tree[level-1]]
+
+
+    # Make a Merkle proof for some index
+    def proof(self, index):
+
+
+
+
+
+
+
+
+
+
 
 
 
