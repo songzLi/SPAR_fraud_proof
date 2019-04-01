7808eb5de4e0c50bd815e8aab1789fe7bd6864c8
fishermanymc <fishermanymc@gmail.com>
2019-04-01 20:05:09 +1100

    updated cost comparison. "Header + sampling" cost is compared agaisnt incorrect coding proof cost.

--- comparisions/cost_comparison/cost_comparison.py
+++ comparisions/cost_comparison/cost_comparison.py
@@ -26,7 +26,7 @@ def cost(block_size, data_symbol_size, batch_factor, hash_size, code_rate,
         merkel_proof_size_spar = B_hash * num_layers_spar
 
         # 2D-RS costs
-        header_2drs = int(np.ceil(np.sqrt(K))) * 4 * hash_size
+        header_2drs = 2 * sqrt_N * hash_size
         num_samples_2drs = num_samples(target_prob, stopping_ratio_2drs, N)
         sampling_bytes_2drs = \
             num_samples_2drs * (B_data + merkel_proof_size_2drs)
@@ -85,49 +85,96 @@ def block_size_vs_cost(block_size_range, data_symbol_size_range,
     schemes = ['2drs', 'spar']
 
     # plot absolute costs
-    x_vectors = [block_size_range] * 2 + [block_size_range[idx:]]
-    x_label = 'block size (MB)'
-    colors = ['b', 'g', 'r']
-    for title in titles:
-        y_vectors = [results[title + '_' + s] / KB for s in schemes] + \
-            [results[title + '_' + schemes[0]][idx:] / KB]
-        labels = schemes + [None]
-        title = title + ' size'
-        y_label = title + ' (kB)'
-        myPlot(x_vectors, y_vectors, colors, labels, x_label, y_label, title,
-               scale='log')
+    # x_vectors = [block_size_range] * 2 + [block_size_range[idx:]]
+    # x_label = 'block size (MB)'
+    # colors = ['b', 'g', 'r']
+    # for title in titles:
+    #     y_vectors = [results[title + '_' + s] / KB for s in schemes] + \
+    #         [results[title + '_' + schemes[0]][idx:] / KB]
+    #     labels = schemes + [None]
+    #     title = title + ' size'
+    #     y_label = title + ' (kB)'
+    #     myPlot(x_vectors, y_vectors, colors, labels, x_label, y_label, title,
+    #            scale='log')
 
-    # plot costs normalized to block size
-    x_label = 'block size (MB)'
-    colors = ['b', 'g', 'r']
-    for title in titles:
-        y_vectors = [results[title + '_' + s] / block_size_range / MB
-                     for s in schemes] + \
-                    [results[title + '_' + schemes[0]][idx:] /
-                     block_size_range[idx:] / MB]
-        labels = schemes + [None]
-        title = title + ' to block size ratio'
-        y_label = title
-        myPlot(x_vectors, y_vectors, colors, labels, x_label, y_label, title,
-               scale='log')
+    # # plot costs normalized to block size
+    # x_label = 'block size (MB)'
+    # colors = ['b', 'g', 'r']
+    # for title in titles:
+    #     y_vectors = [results[title + '_' + s] / block_size_range / MB
+    #                  for s in schemes] + \
+    #                 [results[title + '_' + schemes[0]][idx:] /
+    #                  block_size_range[idx:] / MB]
+    #     labels = schemes + [None]
+    #     title = title + ' to block size ratio'
+    #     y_label = title
+    #     myPlot(x_vectors, y_vectors, colors, labels, x_label, y_label, title,
+    #            scale='log')
+    plot_scatter(results, schemes)
+
+
+def plot_scatter(results, schemes):
+    x_min = 1e15
+    x_max = 0
+    y_min = 1e15
+    y_max = 0
+    header_sample = {}
+    proof = {}
+    for scheme in schemes:
+        header_sample[scheme] = (results['header' + '_' + scheme] +
+                                 results['sampling_bytes' + '_' + scheme]) / KB
+        proof[scheme] = (results['failed_parity_proof' + '_' + scheme]) / KB
+        plt.plot((results['failed_parity_proof' + '_' + scheme]) / KB,
+                 (results['header' + '_' + scheme] +
+                  results['sampling_bytes' + '_' + scheme]) / KB,
+                 label=scheme, linewidth=3,
+                 marker='o', markerfacecolor='w', markersize=8)
+        x_min = np.min([x_min, np.min(results['failed_parity_proof' + '_' +
+                                              scheme])])
+        x_max = np.max([x_max, np.max(results['failed_parity_proof' + '_' +
+                                              scheme])])
+        y_min = np.min([y_min, np.min(results['header' + '_' + scheme] +
+                                      results['sampling_bytes' + '_' +
+                                              scheme])])
+        y_max = np.max([y_max, np.max(results['header' + '_' + scheme] +
+                                      results['sampling_bytes' + '_' +
+                                              scheme])])
+    axis_min = np.min([x_min, y_min]) / KB / 2
+    axis_max = np.max([x_max, y_max]) / KB * 1.2
+    plt.xlim((axis_min, axis_max))
+    plt.ylim((axis_min, y_max * 1.1 / KB))
+    plt.plot([axis_min, axis_max], [axis_min, axis_max], color='r',
+             label='x=y')
+    plt.plot([proof[s] for s in schemes],
+             [header_sample[s] for s in schemes], color='c')
+    for x, y, size in zip(proof['2drs'],
+                          header_sample['2drs'], block_size_range):
+        plt.text(x * 0.9, y * 1.2, str(int(size / MB)) + 'MB')
+    plt.legend(loc='best')
+    plt.grid()
+    plt.xlabel('Failed-parity proof size (KB)', fontsize=16)
+    plt.ylabel('Header + sampling cost (KB)', fontsize=16)
+    plt.tight_layout()
+    plt.show()
 
 
 def myPlot(x_vectors, y_vectors, colors, data_labels, xlabel, ylabel, title,
            scale='log'):
-    for x_vec, y_vec, label, c in zip(x_vectors,
-                                      y_vectors, data_labels, colors):
-        if scale == 'log':
-            plt.semilogy(x_vec, y_vec, label=label, color=c, linewidth=3,
-                         marker='o', markersize=8, markerfacecolor='w')
-        else:
-            plt.plot(x_vec, y_vec, label=label, color=c, linewidth=3,
-                     marker='o', markersize=8, markerfacecolor='w')
-    plt.legend(loc='best')
-    plt.grid()
-    plt.xlabel(xlabel, fontsize=14)
-    plt.ylabel(ylabel, fontsize=14)
-    plt.title(title, fontsize=16)
-    plt.show()
+    # for x_vec, y_vec, label, c in zip(x_vectors,
+    #                                   y_vectors, data_labels, colors):
+    #     if scale == 'log':
+    #         plt.semilogy(x_vec, y_vec, label=label, color=c, linewidth=3,
+    #                      marker='o', markersize=8, markerfacecolor='w')
+    #     else:
+    #         plt.plot(x_vec, y_vec, label=label, color=c, linewidth=3,
+    #                  marker='o', markersize=8, markerfacecolor='w')
+    # plt.legend(loc='best')
+    # plt.grid()
+    # plt.xlabel(xlabel, fontsize=14)
+    # plt.ylabel(ylabel, fontsize=14)
+    # plt.title(title, fontsize=16)
+    # plt.show()
+    pass
 
 
 def block_size_vs_cost_cross(block_size_range, data_symbol_size_range,
@@ -163,7 +210,6 @@ def block_size_vs_cost_cross(block_size_range, data_symbol_size_range,
     # compute when 2D-RS has to use a GF > 256
     N_threshold = 65536
     for idx in range(len(block_size_range)):
-        print(data_symbol_size_range[idx])
         block_size_threshold = \
             N_threshold * data_symbol_size_range[idx] * code_rate
         if block_size_range[idx] > block_size_threshold:
@@ -174,31 +220,33 @@ def block_size_vs_cost_cross(block_size_range, data_symbol_size_range,
     schemes = ['2drs', 'spar']
 
     # plot absolute costs
-    x_vectors = [block_size_range] * 2 + [block_size_range[idx:]]
-    x_label = 'block size (MB)'
-    colors = ['b', 'g', 'r']
-    for title in titles:
-        y_vectors = [results[title + '_' + s] / KB for s in schemes] + \
-            [results[title + '_' + schemes[0]][idx:] / KB]
-        labels = schemes + [None]
-        title = title + ' size'
-        y_label = title + ' (kB)'
-        myPlot(x_vectors, y_vectors, colors, labels, x_label, y_label, title,
-               scale='log')
+    # x_vectors = [block_size_range] * 2 + [block_size_range[idx:]]
+    # x_label = 'block size (MB)'
+    # colors = ['b', 'g', 'r']
+    # for title in titles:
+    #     y_vectors = [results[title + '_' + s] / KB for s in schemes] + \
+    #         [results[title + '_' + schemes[0]][idx:] / KB]
+    #     labels = schemes + [None]
+    #     title = title + ' size'
+    #     y_label = title + ' (kB)'
+    #     myPlot(x_vectors, y_vectors, colors, labels, x_label, y_label, title,
+    #            scale='log')
 
-    # plot costs normalized to block size
-    x_label = 'block size (MB)'
-    colors = ['b', 'g', 'r']
-    for title in titles:
-        y_vectors = [results[title + '_' + s] / block_size_range / MB
-                     for s in schemes] + \
-                    [results[title + '_' + schemes[0]][idx:] /
-                     block_size_range[idx:] / MB]
-        labels = schemes + [None]
-        title = title + ' to block size ratio'
-        y_label = title
-        myPlot(x_vectors, y_vectors, colors, labels, x_label, y_label, title,
-               scale='log')
+    # # plot costs normalized to block size
+    # x_label = 'block size (MB)'
+    # colors = ['b', 'g', 'r']
+    # for title in titles:
+    #     y_vectors = [results[title + '_' + s] / block_size_range / MB
+    #                  for s in schemes] + \
+    #                 [results[title + '_' + schemes[0]][idx:] /
+    #                  block_size_range[idx:] / MB]
+    #     labels = schemes + [None]
+    #     title = title + ' to block size ratio'
+    #     y_label = title
+    #     myPlot(x_vectors, y_vectors, colors, labels, x_label, y_label, title,
+    #            scale='log')
+
+    plot_scatter(results, schemes)
 
 
 def num_samples(target_prob, stopping_ratio, N):
@@ -206,7 +254,7 @@ def num_samples(target_prob, stopping_ratio, N):
     prob = 1
     s = 1
     while True:
-        prob *= (K_hide / N)
+        prob *= ((N - K_hide) / N)
         if prob <= target_prob:
             return s
         s += 1
@@ -216,13 +264,13 @@ def num_samples(target_prob, stopping_ratio, N):
 block_size_range = 2 ** np.array([20, 21, 22, 23, 24, 25, 26, 27, 28])
 data_symbol_size = 256
 data_symbol_size_range = [data_symbol_size] * len(block_size_range)
-batch_factor = 16
+batch_factor = 8
 hash_size = 32
 code_rate = 0.25
 stopping_ratio_2drs = 0.25
 stopping_ratio_spar = 0.124
 target_prob = 0.01
-parity_size = 6
+parity_size = 8
 
 
 block_size_vs_cost(block_size_range, data_symbol_size_range, batch_factor,
---
+++ comparisions/decoding_speed/decoding_time.py
@@ -0,0 +1,40 @@
+import poly_utils as pu
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+
+
+def decoding_time_2drs(K, R):
+    total_time = 0
+    num_rows = K
+
+    for n in range(num_rows):
+        # print('decoding row-' + str(n))
+        x = np.random.randint(0, 65535, K)
+        idx = np.random.permutation(range(int(K / R)))[:K]
+        ts = time.time()
+        pu.lagrange_interp(x, idx)
+        te = time.time()
+        total_time += (te - ts)
+    print('decoding time:', total_time, 'seconds')
+    return total_time
+
+
+dt = []
+R = 0.5
+K_group = [128 * i for i in range(1, 11)]
+result = {}
+result['K_group'] = K_group
+for K in K_group:
+    print(K)
+    dt.append(decoding_time_2drs(K, R))
+    result['decoding_time'] = dt
+    with open('RS_decoding.pickle', 'wb') as handle:
+        pickle.dump(result, handle)
+
+with open('RS_decoding.pickle', 'rb') as handle:
+    result = pickle.load(handle)
+
+plt.plot(result['K_group'], result['decoding_time'])
+plt.show()
